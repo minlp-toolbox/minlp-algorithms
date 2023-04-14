887d778fc5ac26d97f75570922e422d56c795297
Wim Van Roy <wim.vr@hotmail.com>
2023-04-14 10:39:22 +0200

    Small cleanup

--- benders_exp/GBD.py
+++ benders_exp/GBD.py
@@ -1,12 +1,12 @@
-from utils import nlp
+from benders_exp.utils import Nlp
 import casadi as ca
 import numpy as np
 
 # ---- slave -----
 
-c = [4.1, 4] # Center point
-Z = [-10, 10] # Box constraint
-Y = [0, 10] # Box constraint
+c = [4.1, 4]  # Center point
+Z = [-10, 10]  # Box constraint
+Y = [0, 10]  # Box constraint
 
 lam = 1000
 r = 3
@@ -15,21 +15,22 @@ y = ca.SX.sym('y', 2, 1)
 z = ca.SX.sym('z', 1, 1)
 
 # Slave problem is called (1-y) in the paper, the value of which is v(y)
-slave = nlp('slave')
+slave = Nlp('slave')
 slave.var = ca.vertcat(z)
 slave.f = (y[0] - c[0])**2 + (y[1] - c[1])**2 + lam * z
 slave.cons = [(-np.inf, -z, 0),
               (-np.inf, y[0]**2 + y[1]**2 - r**2 - z, 0),
               (Z[0], z, Z[1])]
 slave.parameter = y
-slave.solver_opt.update({'ipopt':{'print_level':0},'print_time':0})
+slave.solver_opt.update({'ipopt': {'print_level': 0}, 'print_time': 0})
 slave.build()
 
-v = lambda p: slave.solve(p)
+
+def v(p): return slave.solve(p)
+
 
 # ---- master -----
-
-master = nlp('master')
+master = Nlp('master')
 
 y0 = ca.SX.sym('y0')
 mu = ca.SX.sym('mu', 2, 1)
@@ -43,12 +44,12 @@ L1 = (y[0] - c[0])**2 + (y[1] - c[1])**2 + \
 L2 = nu[1] * (y[0]**2 + y[1]**2 - r**2) - Z[1]
 
 # They are linearized
-L1_lin = nlp.linearize(L1, [y, mu], y, 'L1_lin')
-L2_lin = nlp.linearize(L2, [y, nu], y, 'L2_lin')
+L1_lin = Nlp.linearize(L1, [y, mu], y, 'L1_lin')
+L2_lin = Nlp.linearize(L2, [y, nu], y, 'L2_lin')
 
 # master problem is defined
 master.solver_name = 'bonmin'
-master.solver_opt = {'discrete':[False, True, True]}
+master.solver_opt = {'discrete': [False, True, True]}
 master.var = ca.vertcat(y0, y)
 master.f = y0
 master.cons = [(Y[0], y, Y[1])]
@@ -61,13 +62,13 @@ v(y_bar)
 mu_bar = slave.solution['lam_g'][0:mu.shape[0]]
 UBD = slave.solution['f']
 LBD = -np.inf
-eps = 1e-3 # tolerance
+eps = 1e-3  # tolerance
 iteration = 0
 
 while LBD + eps <= UBD:
     iteration += 1
     successful = slave.return_stats()['success']
-    
+
     if successful:
         # The new cut is added
         UBD = slave.solution['f']
@@ -76,10 +77,10 @@ while LBD + eps <= UBD:
         # the violated constraints are added
         nu_bar = slave.solution['g'][0:mu.shape[0]] > 0
         master.cons += [(-np.inf, L2_lin(y, y_bar, nu_bar), 0)]
-        
+
     master.build()
     master.solve()
-    
+
     LBD = master.solution['x'][0]
     y_bar = master.solution['x'][1:]
     v(y_bar)
--- benders_exp/utils.py
+++ benders_exp/utils.py
@@ -105,14 +105,13 @@ class DebugCallBack(ca.Callback):
         x = arg[CALLBACK_INPUTS["x"]]
         self.save(x)
         return [0]
-    
-class nlp():
-    """
-    Generate an nlp class using CasADi
-    """
+
+
+class Nlp:
+    """Generate an nlp class using CasADi."""
+
     def __init__(self, name):
-        """Initial values"""
-        
+        """Initialize values."""
         self.parameter = ca.SX.sym('p')
         self.var = ca.SX.sym('x')
         self.var_0 = ca.DM.zeros(self.var.shape)
@@ -121,51 +120,51 @@ class nlp():
         self.solver_name = 'ipopt'
         self.solver_opt = {}
         self.name = name
-        
+
     def build(self):
-        """Build the nlp"""
-        self.g, self.lbg, self.ubg = self.g_lb_ub(self.cons)  
-        self.nlp_definition = {'x': self.var, 'f':self.f, 
-                               'g':self.g, 'p':self.parameter}
-        
-        self.opt = ca.nlpsol(self.name, self.solver_name, 
+        """Build the nlp."""
+        self.g, self.lbg, self.ubg = self.g_lb_ub(self.cons)
+        self.nlp_definition = {'x': self.var, 'f': self.f,
+                               'g': self.g, 'p': self.parameter}
+
+        self.opt = ca.nlpsol(self.name, self.solver_name,
                              self.nlp_definition, self.solver_opt)
-    
-    def solve(self, parameter_value = 0):
-        """Solve the nlp using the solver specified"""
-        self.solution = self.opt(x0 = self.var_0, 
-                                 lbg = self.lbg, ubg = self.ubg, 
-                                 p = parameter_value)
+
+    def solve(self, parameter_value=0):
+        """Solve the nlp using the solver specified."""
+        self.solution = self.opt(x0=self.var_0,
+                                 lbg=self.lbg, ubg=self.ubg,
+                                 p=parameter_value)
+
     def return_stats(self):
-        """return the stats of the solver"""
+        """Return the stats of the solver."""
         return self.opt.stats()
-    
+
     @staticmethod
     def linearize(exp, var, var_lin, name):
         """
-        return a function which is linearization of a function from var to exp.
-        since var might include parameters, var_lin specifies the linearization
+        Return a function which is linearization of a function from var to exp.
+
+        Since var might include parameters, var_lin specifies the linearization
         variable. Example:
-            
+
             f(x, y) = y * sin(x)
             F_l = linearize(y * sin(x), [x, y], x)
             F_l(x, x0, y0) = y0 * sin(x0) + y0 * cos(x0) * (x - x0)
         """
-        
         func = ca.Function('func', var, [exp])
         func_grad = ca.Function('func_grad', var, [ca.jacobian(exp, var_lin)])
-        
+
         tmp_var = ca.SX.sym('var', var_lin.shape)
         exp_lin = func(*var) + func_grad(*var) @ (tmp_var - var_lin)
-        
+
         return ca.Function(name, [tmp_var] + var, [exp_lin])
-        
+
     @staticmethod
     def g_lb_ub(cons):
-        """generate g based on a list of [(lbg, g, ubg)] constraints."""
-        
+        """Generate g based on a list of [(lbg, g, ubg)] constraints."""
         g = ca.vertcat(*[c[1] for c in cons])
         lbg = ca.vertcat(*[x[0]*np.ones(x[1].shape) for x in cons])
         ubg = ca.vertcat(*[x[2]*np.ones(x[1].shape) for x in cons])
-        
+
         return g, lbg, ubg
