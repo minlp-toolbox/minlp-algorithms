272066229709cf85fcc9fc5312a9c8f736e8b9c4
Andrea Ghezzi <andrea.ghezzi@imtek.uni-freiburg.de>
2023-04-26 16:16:01 +0200

    add double pipe problem

--- benders_exp/problems/overview.py
+++ benders_exp/problems/overview.py
@@ -54,9 +54,43 @@ def create_dummy_problem_2():
                      _ubg=ubg, _lbg=lbg, p=[3], solved=True)
     return problem, data
 
+def create_double_pipe_problem(p_val=[1, 5, 1, 10]):
+
+    y = CASADI_VAR.sym("y", 2)
+    z = CASADI_VAR.sym("z", 2)
+    x0 = np.array([0, 0, 0, 0])
+    x = ca.vertcat(*[y, z])
+    idx_x_bin = [0, 1]
+
+    alpha = CASADI_VAR.sym("alpha", 2)
+    r = CASADI_VAR.sym("r", 1)
+    gamma = CASADI_VAR.sym("gamma", 1)
+    p = ca.vertcat(*[alpha, r, gamma])
+
+    f = alpha[0] * y[0] * z[0] + alpha[1] * y[1]
+    g = ca.vertcat(*[
+        y[0] * z[0] + y[1] * z[1] - r,
+        gamma - z[1]
+        ])
+    lbg = np.array([0, 0])
+    ubg = np.array([ca.inf, ca.inf])
+    lbx = np.array([0, 0, 0, -ca.inf])
+    ubx = np.array([1, 1, ca.inf, ca.inf])
+
+    problem = MinlpProblem(x=x, f=f, g=g, p=p, idx_x_bin=idx_x_bin)
+    data = MinlpData(x0=x0, _ubx=ubx, _lbx=lbx,
+                     _ubg=ubg, _lbg=lbg, p=p_val, solved=True)
+    return problem, data
+
 
 PROBLEMS = {
     "dummy": create_dummy_problem,
     "dummy2": create_dummy_problem_2,
-    "orig": extract_solarsys
+    "orig": extract_solarsys,
+    "doublepipe": create_double_pipe_problem
 }
+
+
+if __name__ == '__main__':
+    prob, data = create_double_pipe_problem()
+    # prob, data = create_dummy_problem()
