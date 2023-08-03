d6b358e8d47ff02a8b6f8a29d69616eca862d611
Andrea Ghezzi <andrea.ghezzi@imtek.uni-freiburg.de>
2023-08-03 16:28:18 +0200

    add benders mix to test

--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -8,7 +8,7 @@ from benders_exp.problems import check_solution
 
 options = [
     (solver, problem)
-    for solver in ["benders", "bendersqp", "bonmin", "benders_tr"]
+    for solver in ["benders", "bendersqp", "bonmin", "benders_tr", "benders_trm"]
     for problem in PROBLEMS.keys()
     if (
         problem not in ["orig", "doubletank2", "doubletank",
