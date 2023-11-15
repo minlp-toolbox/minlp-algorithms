7824c49f91f1d30a0c1044778e7ab4d01b647757
Wim Van Roy <wim.vr@hotmail.com>
2023-11-15 13:37:03 +0100

    Add checking any equal.

---
+++ benders_exp/utils/debugtools.py
@@ -0,0 +1,21 @@
+import numpy as np
+from benders_exp.solvers import MinlpProblem, MinlpData
+from benders_exp.defines import EPS
+
+
+class CheckNoDuplicate:
+    """Check duplicates."""
+
+    def __init__(self, problem: MinlpProblem):
+        """Check if no duplicates passes through."""
+        self.idx_x_bin = problem.idx_x_bin
+        self.old = []
+
+    def __call__(self, nlpdata: MinlpData):
+        """Check if no old solutions pass through."""
+        for sol in nlpdata.prev_solutions:
+            new = sol['x'][self.idx_x_bin]
+            for el in self.old:
+                if np.allclose(el, new, equal_nan=False, atol=EPS):
+                    raise Exception("Duplicate Found.")
+            self.old.append(new)
